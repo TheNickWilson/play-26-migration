370d0aa0a360e7cf27326eb36cf1bbe530986398
Michael Wolfendale <michael.wolfendale@gmail.com>
2018-11-13 08:40:22 +0000

    Updates to SessionIdFilter

--- app/filters/SessionIdFilter.scala
+++ app/filters/SessionIdFilter.scala
@@ -22,6 +22,8 @@ import akka.stream.Materializer
 import com.google.inject.Inject
 import play.api.http.HeaderNames
 import play.api.mvc._
+import play.api.mvc.request.{Cell, RequestAttrKey}
+import play.api.routing.Router.Attrs
 import uk.gov.hmrc.http.{SessionKeys, HeaderNames => HMRCHeaderNames}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -29,50 +31,42 @@ import scala.concurrent.{ExecutionContext, Future}
 class SessionIdFilter (
                         override val mat: Materializer,
                         uuid: => UUID,
+                        sessionCookieBaker: SessionCookieBaker,
                         implicit val ec: ExecutionContext
                       ) extends Filter {
 
   @Inject
-  def this(mat: Materializer, ec: ExecutionContext) {
-    this(mat, UUID.randomUUID(), ec)
+  def this(mat: Materializer, ec: ExecutionContext, sessionCookieBaker: SessionCookieBaker) {
+    this(mat, UUID.randomUUID(), sessionCookieBaker, ec)
   }
 
-  override def apply(f: (RequestHeader) => Future[Result])(rh: RequestHeader): Future[Result] = {
+  override def apply(f: RequestHeader => Future[Result])(rh: RequestHeader): Future[Result] = {
 
     lazy val sessionId: String = s"session-$uuid"
 
     if (rh.session.get(SessionKeys.sessionId).isEmpty) {
 
-      val cookies: String = {
-
-        val session: Session =
-          rh.session + (SessionKeys.sessionId -> sessionId)
-
-        val cookies =
-          rh.cookies ++ Seq(Session.encodeAsCookie(session))
-
-        Cookies.encodeCookieHeader(cookies.toSeq)
-      }
-
       val headers = rh.headers.add(
-        HMRCHeaderNames.xSessionId -> sessionId,
-        HeaderNames.COOKIE -> cookies
+        HMRCHeaderNames.xSessionId -> sessionId
       )
 
-      f(rh.withHeaders(headers)).map {
-        result =>
+      val session = rh.session + (SessionKeys.sessionId -> sessionId)
 
-          val cookies =
-            Cookies.fromSetCookieHeader(result.header.headers.get(HeaderNames.SET_COOKIE))
-
-          val session = Session.decodeFromCookie(cookies.get(Session.COOKIE_NAME)).data
-            .foldLeft(rh.session) {
-              case (m, n) =>
-                m + n
-            }
-
-          result.withSession(session + (SessionKeys.sessionId -> sessionId))
-      }
+      f(rh.withHeaders(headers).addAttr(RequestAttrKey.Session, Cell(session)))//.map {
+//        result =>
+//
+//          val cookies =
+//            Cookies.fromSetCookieHeader(result.header.headers.get(HeaderNames.SET_COOKIE))
+//
+//          val updatedSession = Session.decodeFromCookie(cookies.get(sessionCookieBaker.COOKIE_NAME)).data
+//            .foldLeft(session) {
+//              case (m, n) =>
+//                m + n
+//            }
+//
+//          result.withSession(updatedSession + (SessionKeys.sessionId -> sessionId))
+//          result
+//      }
     } else {
       f(rh)
     }
